9b535ef3e12ae43b39a6dc938254b0b533892d5c
Vika Nosova <viktoriianosova@hotmail.com>
2018-06-27 19:19:52 +0800

    Fix input on React

--- src/components/Label.js
+++ src/components/Label.js
@@ -3,6 +3,16 @@ import React from 'react';
 class Label extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            value: '1'
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
     }
 
     render() {
@@ -10,7 +20,7 @@ class Label extends React.Component {
             <label>
                 <span>{this.props.content.name}</span>
                 <span>{this.props.content.minValue}</span>
-                <input type="range" min={this.props.content.minValue} max={this.props.content.maxValue} value={this.props.content.defaultValue} />
+                <input type="range" min={this.props.content.minValue} max={this.props.content.maxValue} value={this.state.value} onChange={this.handleChange} />
                 <span>{this.props.content.maxValue}</span>
             </label>
         )
